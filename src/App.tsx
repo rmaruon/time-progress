9e99330f5e76bc0fe7d45eb827cacbaea8ee3778
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-09-26 16:17:57 +0900

    update: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import { makeStyles, Typography } from "@material-ui/core";
+import { Progress } from "./Progress";
+import { calcYearProgress, calcMonthProgress, calcDayProgress } from "./utils";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: "center",
+    maxWidth: "800px",
+    margin: "0 auto",
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: "4rem",
+    margin: "80px 0",
+    color: "#233142",
+  },
+  container: {
+    margin: "100px auto",
+    maxWidth: "600px",
+  },
+}));
+
+const App = () => {
+  const [time, setTime] = useState(new Date().getTime());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date().getTime());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  const { yearProgress, monthProgress, dayProgress } = useMemo(() => {
+    const yearProgress = calcYearProgress(time);
+    const monthProgress = calcMonthProgress(time);
+    const dayProgress = calcDayProgress(time);
+    return { yearProgress, monthProgress, dayProgress };
+  }, [time]);
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Typography variant="h1" className={classes.header}>
+        Time Progress
+      </Typography>
+      <div className={classes.container}>
+        <Progress title="Year" value={yearProgress}></Progress>
+        <Progress title="Month" value={monthProgress}></Progress>
+        <Progress title="Day" value={dayProgress}></Progress>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
